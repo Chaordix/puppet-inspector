64fefc5da39b3eb9e2b7ed0941583273fdb4b67d
chris.funderburg <chris.funderburg@di2e.net>
2021-05-05 11:44:30 -0400

    add tabs to match line and use single-quotes

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,7 +18,7 @@ class inspector::install inherits inspector {
     }
     'CentOS': {
       exec { 'download_inspector-agent':
-        command => "/usr/bin/wget -N https://d1wk0tztpsntt1.cloudfront.net/linux/latest/install -O /opt/inspector-install.sh",
+        command => '/usr/bin/wget -N https://d1wk0tztpsntt1.cloudfront.net/linux/latest/install -O /opt/inspector-install.sh',
         path    => '/bin:/usr/bin:/usr/local/bin:/usr/sbin',
         creates => '/opt/inspector-install.sh',
       }
@@ -28,20 +28,20 @@ class inspector::install inherits inspector {
       ~> file_line { 'update yum reinstall':
         path    => '/opt/inspector-install.sh',
         replace => true,
-        line    => "yum reinstall -y --nogpgcheck \"${package_path}\"",
-        match   => "yum reinstall -y \"${package_path}\"",
+        line    => '         yum reinstall -y --nogpgcheck \"${package_path}\"',
+        match   => '         yum reinstall -y \"${package_path}\"',
       }
       ~> file_line { 'update yum install':
         path    => '/opt/inspector-install.sh',
         replace => true,
-        line    => "yum install -y --nogpgcheck \"${package_path}\"",
-        match   => "yum install -y \"${package_path}\"",
+        line    => '          yum install -y --nogpgcheck \"${package_path}\"',
+        match   => '          yum install -y \"${package_path}\"',
       }
       ~> file_line { 'update yum downgrade':
         path    => '/opt/inspector-install.sh',
         replace => true,
-        line    => "yum downgrade -y --nogpgcheck \"${package_path}\"",
-        match   => "yum downgrade -y \"${package_path}\"",
+        line    => "          yum downgrade -y --nogpgcheck \"${package_path}\"",
+        match   => "          yum downgrade -y \"${package_path}\"",
       }
       ~> exec { 'install_inspector-agent':
         command     => '/opt/inspector-install.sh',
